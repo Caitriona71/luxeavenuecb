c4ea294ba48bf9eb57177bfdae0fe10b161de640
Caitriona71 <caitrionaboyle@ymail.com>
2023-05-13 21:45:14 +0000

    Product Admin - Editing Products

--- profiles/views.py
+++ profiles/views.py
@@ -16,6 +16,8 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Your profile has been updated')
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
 
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
