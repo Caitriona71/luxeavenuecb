4e2834428f4f425cd4e602cba38636272a4f5dcd
Caitriona71 <caitrionaboyle@ymail.com>
2023-05-14 17:56:55 +0000

    Product Admin - fixing the image field Part 2

--- products/views.py
+++ products/views.py
@@ -7,7 +7,6 @@ from django.db.models.functions import Lower
 from .models import Product, Category
 from .forms import ProductForm
 
-
 # Create your views here.
 
 
@@ -73,7 +72,7 @@ def product_detail(request, product_id):
     return render(request, 'products/product_detail.html', context)
 
 
-# Login required
+@login_required
 def add_product(request):
     """ Add a product to the store """
     if not request.user.is_superuser:
@@ -99,7 +98,7 @@ def add_product(request):
     return render(request, template, context)
 
 
-# Login required
+@login_required
 def edit_product(request, product_id):
     """ Edit a product in the store """
     if not request.user.is_superuser:
@@ -128,7 +127,7 @@ def edit_product(request, product_id):
     return render(request, template, context)
 
 
-# Login required
+@login_required
 def delete_product(request, product_id):
     """ Delete a product from the store """
     if not request.user.is_superuser:
@@ -139,3 +138,4 @@ def delete_product(request, product_id):
     product.delete()
     messages.success(request, 'Product deleted!')
     return redirect(reverse('products'))
+
--- profiles/views.py
+++ profiles/views.py
@@ -7,7 +7,7 @@ from .forms import UserProfileForm
 from checkout.models import Order
 
 
-# Login required
+@login_required
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
@@ -48,4 +48,3 @@ def order_history(request, order_number):
     }
 
     return render(request, template, context)
-    
