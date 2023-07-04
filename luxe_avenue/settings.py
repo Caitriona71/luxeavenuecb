eb9a08db84943b023ced634928cb3fe3e0e61aa5
Caitriona71 <caitrionaboyle@ymail.com>
2023-07-04 11:57:24 +0000

    Add disallowed host to allowed hosts in settings.py

--- luxe_avenue/settings.py
+++ luxe_avenue/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['8000-caitriona71-luxeavenuec-dp7lq9hrhfd.ws-eu100.gitpod.io', 'luxeavenuecb.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-caitriona71-luxeavenuec-dp7lq9hrhfd.ws-eu101.gitpod.io', 'luxeavenuecb.herokuapp.com', 'localhost']
 
 
 # Application definition
